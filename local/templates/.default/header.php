7486feeb9a6cad5d4f1ba451e728d9d5b01404fe
Jerff <info@jdjiwi.ru>
2017-02-12 07:03:27 +0000

    fix

--- local/templates/.default/header.php
+++ local/templates/.default/header.php
@@ -3,6 +3,6 @@
 <!DOCTYPE html>
 <html lang="<?=LANGUAGE_ID?>">
 <head>
-    <? $APPLICATION->IncludeComponent("bitrix:main.include", "", array_merge($arDefIncludeParams,array("PATH" => SITE_DIR."include/header/head.script.php")), false, array("HIDE_ICONS"=>"Y")); ?>
+    <? $APPLICATION->IncludeComponent("bitrix:main.include", "", array("PATH" => SITE_DIR."include/header/head.script.php"), false, array("HIDE_ICONS"=>"Y")); ?>
 </head>
 <body>
